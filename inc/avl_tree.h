cb9a64f0030fe7a11de1446f331456faeb40f8c7
Halis <67235745+nothing-githb@users.noreply.github.com>
2023-08-19 08:23:29 +0300

    avl array

--- inc/avl_tree.h
+++ inc/avl_tree.h
@@ -1,6 +1,10 @@
 /**
  * avl tree implementation
  * 
+ * int _cmp(unsigned long p1, unsigned long p2)
+ * {
+ *   return ((p2 < p1) - (p1 < p2));
+ * }
  * int main()
  * {
  *      unsigned long array[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
